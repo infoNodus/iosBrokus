c6dec6e37d59e752c86e07d7c44f1f12124e134d
IOS Nodus <e.ios@outlook.com>
2013-10-16 10:16:41 -0500

    oooo

--- Brokus/BrockUs/BUAppDelegate.h
+++ Brokus/BrockUs/BUAppDelegate.h
@@ -3,7 +3,6 @@
 //  BrockUs
 //  Created by HUB 3C 2 on 25/09/13.
 //  Copyright (c) 2013 Nodus. All rights reserved.
-
 #import <UIKit/UIKit.h>
 #import "BULoginViewController.h"
 #import "BURegistroViewController.h"

a8c77ebcb808c250db6e5b1aabfcdddbd879bd2f
IOS Nodus <e.ios@outlook.com>
2013-10-21 12:05:38 -0500

    sdsdsd

--- Brokus/BrockUs/BULoginViewController.m
+++ Brokus/BrockUs/BULoginViewController.m
@@ -19,8 +19,8 @@
 @property (strong) BURegistroViewController *registro; //propiedad para acceder al controlador del registro
 @property (strong)BULoginViewController *log; //propiedad para acceder al controlador de login
 
-@end 
- 
+@end
+
 @implementation BULoginViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -35,19 +35,19 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    [[self UsertInputText ] setDelegate:self];
-    [[self PassInputText] setDelegate:self];
- 
+    [[self UsertInputText ] setDelegate:self];//inicializar el campo de texto para el login del user
+    [[self PassInputText] setDelegate:self];//inicializar el campo de  Contraseña para la constraseña del user
+    
     
     BUAppDelegate * buappdelegate=[[UIApplication sharedApplication]delegate];//Se inicializa una instancia del appdelegate
     context =[buappdelegate managedObjectContext];//asignación del contexto actual
-
-    BUConsultaPublicacion *consulta = [[BUConsultaPublicacion alloc] init];
-    [consulta desactivaPublicacionesCaducadastoContext:context];
+    
+    BUConsultaPublicacion *consulta = [[BUConsultaPublicacion alloc] init];//Se inicializa una instancia del BUConsulta
+    [consulta desactivaPublicacionesCaducadastoContext:context];//asignación del contexto actual
     
     self.perfil=[[BUPerfilEmpresaViewController alloc] initWithNibName:@"BUPerfilEmpresaViewController" bundle:nil];
     self.registro=[[BURegistroViewController alloc] initWithNibName:@"BURegistroViewController" bundle:nil];
- 
+    
 }
 
 //IMPORTANTE ES PARA OCULTAR EL TECLADO ANTES IR AL XIB Y PONER DID ON EXIT ADEMAS DE EN EL .H PONER EN TU INTERFAZ<UITextFieldDelegate>
@@ -65,7 +65,7 @@
     return [emailTest evaluateWithObject:candidate];
 }
 
- 
+
 //Termino de validar el mail
 
 - (BOOL) validaPass: (NSString *) pass{
@@ -88,53 +88,53 @@
         [vacio show];
         self.UsertInputText.clearButtonMode=YES;
         self.UsertInputText.clearsOnBeginEditing=YES;
-
+        
     }
-  
+    
     else if ([_UsertInputText.text length]!=0 && [_PassInputText.text length]==0  )
     {
-            if ([self validateEmail:[_UsertInputText.self text]] ==1)
-            {
-                UIAlertView *passfal = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Llene Campo Password" delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
-                [passfal show];
-                self.UsertInputText.clearButtonMode=YES;
-                self.UsertInputText.clearsOnBeginEditing=YES;
-            }
-            else if([self validateEmail:[_UsertInputText.self text]]==0)
-            {
-                UIAlertView *usermal = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Error en Formato de Email y Llene Campo Password" delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
-                [usermal show];
-                self.UsertInputText.clearButtonMode=YES;
-                self.UsertInputText.clearsOnBeginEditing=YES;
-            }
+        if ([self validateEmail:[_UsertInputText.self text]] ==1)
+        {
+            UIAlertView *passfal = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Llene Campo Password" delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+            [passfal show];
+            self.UsertInputText.clearButtonMode=YES;
+            self.UsertInputText.clearsOnBeginEditing=YES;
+        }
+        else if([self validateEmail:[_UsertInputText.self text]]==0)
+        {
+            UIAlertView *usermal = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Error en Formato de Email y Llene Campo Password" delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+            [usermal show];
+            self.UsertInputText.clearButtonMode=YES;
+            self.UsertInputText.clearsOnBeginEditing=YES;
+        }
     }
     else if ([_UsertInputText.text length]==0 && [_PassInputText.text length]!=0  )
     {
-            if ([self validaPass:[_PassInputText.self text]] ==1)
-            {
-                UIAlertView *passfal = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Llene Campo Email" delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
-                [passfal show];
-                self.UsertInputText.clearButtonMode=YES;
-                self.UsertInputText.clearsOnBeginEditing=YES;
-            }
-            else if ([self validaPass:[_PassInputText.self text]]==0)
-            {
-                UIAlertView *usermal = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Error en Formato de Contraseña y Llene Campo Email" delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
-                [usermal show];
-                self.UsertInputText.clearButtonMode=YES;
-                self.UsertInputText.clearsOnBeginEditing=YES;
-            }
-
+        if ([self validaPass:[_PassInputText.self text]] ==1)
+        {
+            UIAlertView *passfal = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Llene Campo Email" delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+            [passfal show];
+            self.UsertInputText.clearButtonMode=YES;
+            self.UsertInputText.clearsOnBeginEditing=YES;
+        }
+        else if ([self validaPass:[_PassInputText.self text]]==0)
+        {
+            UIAlertView *usermal = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Error en Formato de Contraseña y Llene Campo Email" delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+            [usermal show];
+            self.UsertInputText.clearButtonMode=YES;
+            self.UsertInputText.clearsOnBeginEditing=YES;
+        }
+        
     }
     else if ([_UsertInputText.text length]!=0 && [_PassInputText.text length]!=0  )
-    { 
+    {
         if (([self validateEmail:[_UsertInputText.self text]] ==1) && [self validaPass:[_PassInputText.self text]]==0) //si valida pass es igual a 0 es correcto si es 1 es incorrrecto
         {
-                            self.UsertInputText.clearButtonMode=YES;
-                self.UsertInputText.clearsOnBeginEditing=YES;
+            self.UsertInputText.clearButtonMode=YES;
+            self.UsertInputText.clearsOnBeginEditing=YES;
             
-       //     self.registro.delegate=self;
-         //   [self presentViewController:self.registro animated:YES completion:nil];
+            //     self.registro.delegate=self;
+            //   [self presentViewController:self.registro animated:YES completion:nil];
             
             
             self.ErrrorLabel.text =@"Espere Un Momento Por Favor";
@@ -231,9 +231,9 @@
                     }
                 }
             }
-
             
-
+            
+            
             
         }
         else if (([self validateEmail:[_UsertInputText.self text]] ==0) && [self validaPass:[_PassInputText.self text]]==1)
@@ -245,7 +245,7 @@
             
         }
         else  if (([self validateEmail:[_UsertInputText.self text]] ==1) && [self validaPass:[_PassInputText.self text]]==1) //Si todo es correcto aki se envia la validacion a
-         //la base de  datos y logeamos y acemos todo el proceso de loggin y posinle registro
+            //la base de  datos y logeamos y acemos todo el proceso de loggin y posinle registro
         {
             
             
@@ -270,7 +270,7 @@
             {
                 self.ErrrorLabel.hidden=NO;
                 self.ErrrorLabel.text=@"Error en Usuario o Contraseña";
-               // UIAlertView *usermal = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Usuario ò Passowrd  Incorrecta" delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+                // UIAlertView *usermal = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Usuario ò Passowrd  Incorrecta" delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
                 //[usermal show];
                 //UIAlertView *ise=[[UIAlertView alloc]initWithTitle:@"Error" message:@"Al Parecer no Estas Registrado, Espera..." delegate:self cancelButtonTitle:@"Registrarme" otherButtonTitles:@"Reintentar", nil];
                 
@@ -279,26 +279,26 @@
                 UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"Mensaje" message:@"Al Parecer no estas Registrado o Introduciste mal los Datos" delegate:self cancelButtonTitle:@"Cancelar" otherButtonTitles:@"Registrarse",nil];
                 [alert setTag:1];
                 [alert show];
-             //   [alert release];
+                //   [alert release];
                 
                 
-               //self..delegate=self;
+                //self..delegate=self;
                 
                 //[self presentViewController:self.elegirCiudad animated:YES completion:nil];
                 
-               // self.registro.delegate=self;
+                // self.registro.delegate=self;
                 //[self presentViewController:self.registro animated:YES completion:nil];
-
+                
                 //para regresar     [self dismissViewControllerAnimated:YES completion:nil];
                 
                 
                 //BUPerfilEmpresaViewController *lista=[[BUPerfilEmpresaViewController alloc] initWithNibName:@"BUPerfilEmpresaViewController" bundle:nil];
-               // [self presentViewController:self.registro animated:YES completion:nil];
-             
+                // [self presentViewController:self.registro animated:YES completion:nil];
+                
                 
                 // self.window.rootViewController = lista;
                 
-            //[self.window makeKeyAndVisible];
+                //[self.window makeKeyAndVisible];
             }
             else
             {
@@ -308,94 +308,102 @@
                     usuario=[obj valueForKey:@"usuario"];
                     contrasena=[obj valueForKey:@"contrasena"];
                     
-               
-                
-                if (_PassInputText.text ==contrasena) {
-                    
-                                     
-                    self.ErrrorLabel.hidden=NO;
-                    self.ErrrorLabel.text=@"Password Incorrecta";
-
                     
                     
-                }
-                else{
-                
-               // self.ErrrorLabel.text=[NSString stringWithFormat:@"user: %@, password:%@",usuario,contrasena];
-                
-                  //  user=usuario;
-                    self.ErrrorLabel.hidden=NO;
-                    self.ErrrorLabel.text=@"Usuario Correcto Iniciando";
-                    UIAlertView *usermal = [[UIAlertView alloc] initWithTitle:@"Correcto" message:@"Iniciando..." delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
-                    [usermal show];
-                    
-                    [[NSUserDefaults standardUserDefaults] setValue:self.UsertInputText.text forKey:@"UserBrockus"];
-                    
-                    
-                    [UIView beginAnimations:nil context:NULL];
-                    [UIView setAnimationDuration:1];
-                    [UIView setAnimationTransition:UIViewAnimationTransitionFlipFromRight forView:self.view cache:YES];
-                    [UIView commitAnimations];
-
-                    
-                     self.perfil.delegate=(id)self;
-                    UINavigationController *navContr = [[UINavigationController alloc] initWithRootViewController:self.perfil];
-                    navContr.title=@"Perfil";
-                    
-                   // self.navigationItem.leftBarButtonItem = self.btnSalir;
-                   // self.navigationItem.rightBarButtonItem = self.btnCrearPublicacion;
-                    self.title = @"Perfil";
-
-
-                    [self presentViewController:navContr animated:YES completion:nil];
-                    
-                                         }
+                    if (_PassInputText.text ==contrasena) {
+                        
+                        
+                        self.ErrrorLabel.hidden=NO;
+                        self.ErrrorLabel.text=@"Password Incorrecta";
+                        
+                        
+                        
+                    }
+                    else{
+                        
+                        // self.ErrrorLabel.text=[NSString stringWithFormat:@"user: %@, password:%@",usuario,contrasena];
+                        
+                        //  user=usuario;
+                        self.ErrrorLabel.hidden=NO;
+                        self.ErrrorLabel.text=@"Usuario Correcto Iniciando";
+                        UIAlertView *usermal = [[UIAlertView alloc] initWithTitle:@"Correcto" message:@"Iniciando..." delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+                        [usermal show];
+                        
+                        [[NSUserDefaults standardUserDefaults] setValue:self.UsertInputText.text forKey:@"UserBrockus"];
+                        
+                        
+                        [UIView beginAnimations:nil context:NULL];
+                        [UIView setAnimationDuration:1];
+                        [UIView setAnimationTransition:UIViewAnimationTransitionFlipFromRight forView:self.view cache:YES];
+                        [UIView commitAnimations];
+                        
+                        
+                        self.perfil.delegate=(id)self;
+                        UINavigationController *navContr = [[UINavigationController alloc] initWithRootViewController:self.perfil];
+                        navContr.title=@"Perfil";
+                        
+                        // self.navigationItem.leftBarButtonItem = self.btnSalir;
+                        // self.navigationItem.rightBarButtonItem = self.btnCrearPublicacion;
+                        self.title = @"Perfil";
+                        
+                        
+                        [self presentViewController:navContr animated:YES completion:nil];
+                        
+                    }
                 }
             }
         }
         
     }
-   
-
     
-
+    
+    
+    
     
     
     
     //Termina validacion de email correcto y datos llenos enlos textfield
-
+    
 }
 
 
 
 
 
-
+//objeto ke se comunica cuando un usuario no esta registrado o a introducido mal su password
 - (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
 
 {
+    //si el user introduce mal su password se muestra el mensaje y si presiona en cancelar se da otro oportunidad de ke el user
+    //ingrese bien su user y password
     if (buttonIndex ==0) {
-         NSLog(@"Cancelar: %i, was pressed.", buttonIndex);
+        NSLog(@"Cancelar: %i, was pressed.", buttonIndex);
     }
+    //si el user no esta registrado se le dice ke se registre y se envia al xib de registro de usuarios
     else if (buttonIndex==1){
         NSLog(@"Registrar: %i, was pressed.", buttonIndex);
-        self.registro.delegate=(id)self;
-        [self presentViewController:self.registro animated:YES completion:nil];
-
+        self.registro.delegate=(id)self;//se denota ake xib se e va a enviar
+        [self presentViewController:self.registro animated:YES completion:nil]; //procede a acer la transsion de pantalla al enviar al usuario
+// al xib de registro
     }
-   
+    
 }
-
+//Objeto ke da acceso al registro del usuario para despues ser logeado
 - (IBAction)RegisterBtnTapped:(id)sender {
     
+    //////////////////////////////////////////
     [UIView beginAnimations:nil context:NULL];
     [UIView setAnimationDuration:1];
     [UIView setAnimationTransition:UIViewAnimationTransitionFlipFromRight forView:self.view cache:YES];
     [UIView commitAnimations];
+    ////Estas Lineas de codigo especifican una animacion al acer cambio de xib
+    //cabe señalar ke la linea mas siginificativa es    [UIView setAnimationTransition:UIViewAnimationTransitionFlipFromRight forView:self.view cache:YES];
+    //en la propiedad setAnimationTransicition: y los sig son los tipos de animacion ke se pueden escoger ke son de uivviewtransitionanimation-animacion a selecionar
     
-    self.registro.delegate=(id)self;
-    [self presentViewController:self.registro animated:YES completion:nil];
     
-
+    self.registro.delegate=(id)self;//mediante el delegate ya declarado en el .m del xib del registro
+    [self presentViewController:self.registro animated:YES completion:nil]; //y con esto procedemos a pasar al sig xib con transicion animada al .xib BURegistroViewController
+    
+    
 }
 @end
